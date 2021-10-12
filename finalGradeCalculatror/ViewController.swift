77caf28aa975c7904891a775677663d1f7004acb
Kimberly Herrera <kimherrera601@gmail.com>
2021-10-12 11:25:31 -0600

    MVP

--- finalGradeCalculatror/ViewController.swift
+++ finalGradeCalculatror/ViewController.swift
@@ -7,13 +7,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var textField1: UITextField!
+    
+    @IBOutlet weak var textField2: UITextField!
+    
+    @IBOutlet weak var textField3: UITextField!
+    
+    @IBOutlet weak var resultLable: UILabel!
+    
+    override func viewDidLoad()
+    
+    
+    
+    
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+
+    @IBAction func calculateButton(_ sender: Any)
+    {
+        var x = textField1.text ?? "0"
+        var y = textField2.text ?? "0"
+        var z = textField3.text ?? "0"
+        var a = Float(x) ?? 0.0
+        var b = Float(y) ?? 0.0
+        var c = Float(z) ?? 0.0
+        //A=C
+        //B=D
+        //C=W
+        var result = (b-a*(1-c/100))/(c/100)
+        textField1.resignFirstResponder()
+        textField2.resignFirstResponder()
+        textField3.resignFirstResponder()
+        self.resultLable.text="\(result)%"
+    
+    }
+    @IBAction func clearButton(_ sender: Any)
+    {
+        textField1.text = ""
+        textField2.text = ""
+        textField3.text = ""
+        resultLable.text = "No Result Yet"
+        
+    }
 
 }
 
